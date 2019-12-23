8669822f30402f437ad48948653116c78d20ee28
Tomokazu HIRAI <tomokazu.hirai@gmail.com>
2019-12-24 00:09:10 +0900

    initial commit

---
+++ main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/endpoints"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/mitchellh/go-homedir"
+	"github.com/nlopes/slack"
+	"github.com/spf13/viper"
+)
+
+var (
+	bucketname string = "samplebucket"
+	filename   string = "sample.txt"
+	outputfile string = "/tmp/slack-ansible"
+)
+
+const S3Endpoint = "http://localhost:4572"
+
+func init() {
+	home, err := homedir.Dir()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	viper.AddConfigPath(home)
+	viper.SetConfigName(".slack-ansible")
+
+	viper.AutomaticEnv()
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file: ", viper.ConfigFileUsed())
+	}
+}
+
+func main() {
+	token := viper.GetString("token")
+	api := slack.New(token)
+	os.Exit(run(api))
+}
+
+func run(api *slack.Client) int {
+	rtm := api.NewRTM()
+	go rtm.ManageConnection()
+
+	for {
+		msg := <-rtm.IncomingEvents
+		log.Printf("MSG: %#v\n", msg.Data)
+
+		switch ev := msg.Data.(type) {
+		case *slack.HelloEvent:
+			log.Printf("Start up!")
+
+		case *slack.MessageEvent:
+			if strings.HasPrefix(ev.Text, "!echo") {
+				out, err := exec.Command("sh", "-c", ev.Text[1:]).Output()
+
+				r := bytes.NewReader(out)
+
+				url := S3PutObject(bucketname, filename, r)
+				if err != nil {
+					fmt.Printf("Fatal error : %s \n", err)
+				}
+
+				rtm.SendMessage(rtm.NewOutgoingMessage(url+"\n"+"```"+string(out)+"```", ev.Channel))
+			}
+		}
+	}
+}
+
+func S3PutObject(bucketname, key string, rs io.ReadSeeker) string {
+	s := session.Must(session.NewSession(&aws.Config{
+		Credentials:      credentials.NewStaticCredentials("foo", "var", ""),
+		S3ForcePathStyle: aws.Bool(true),
+		Region:           aws.String(endpoints.UsWest2RegionID),
+		Endpoint:         aws.String(S3Endpoint),
+	}))
+
+	c := s3.New(s, &aws.Config{})
+
+	p := s3.PutObjectInput{
+		Bucket: aws.String(bucketname),
+		Key:    aws.String(key),
+		ACL:    aws.String("public-read"),
+		Body:   rs,
+	}
+
+	_, err := c.PutObject(&p)
+	if err != nil {
+		panic(err)
+	}
+
+	filepath := S3Endpoint + "/" + bucketname + "/" + key
+
+	return filepath
+}
